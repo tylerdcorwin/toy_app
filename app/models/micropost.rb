fb3c6044881dbec56560ebc1872a6a724eab4a93
Tyler Corwin <Tyler.C@figmints.com>
2019-01-17 13:37:49 -0500

    finished toy app

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ApplicationRecord
   #with associations we need to clarify where items belong
   belongs_to :user
-  validates :content, length: { maximum: 140 }
+  validates :content, length: { maximum: 140 },
+                      presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   #a user can have many microposts so there is a association
   #notice that microposts is plural! altough the model [class] is singular
   has_many :microposts
+  validates :user, presence: true
+  validates :micropost, presence: true
 end
